06fa4857096fc91bd15dc2067ab7b487257e0bad
nikolayVv <nikolaivalvasilev@gmail.com>
2023-01-14 12:12:45 +0100

    dynamic IP

--- api/src/main/java/si/fri/rso/samples/deliveries/api/v1/DeliveryApplication.java
+++ api/src/main/java/si/fri/rso/samples/deliveries/api/v1/DeliveryApplication.java
@@ -14,7 +14,7 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 @OpenAPIDefinition(info = @Info(title = "Deliveries API", version = "v1",
         contact = @Contact(email = "nv7834@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing deliveries."),
-        servers = @Server(url = "http://146.190.206.102:8080/"),
+        servers = @Server(url = "http://localhost:8080/"),
         externalDocs = @ExternalDocumentation(description = "Delivery application repository", url = "https://github.com/rso-2022-2023/RSO_S47_Delivery"))
 @ApplicationPath("/v1")
 public class DeliveryApplication extends Application {
